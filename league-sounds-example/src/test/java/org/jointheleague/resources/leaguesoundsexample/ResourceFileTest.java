7789940a1e597529e1aefaebf06c1134e1da757d
Stephen Cerruti <scerruti@otabi.com>
2016-11-19 13:15:30 -0800

    Added example project

---
+++ league-sounds-example/src/test/java/org/jointheleague/resources/leaguesoundsexample/ResourceFileTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2016. The League of Amazing Programmers, All Rights Reservered
+ */
+
+package org.jointheleague.resources.leaguesoundsexample;
+
+import org.junit.Test;
+
+import java.io.InputStream;
+
+import static org.junit.Assert.*;
+
+/**
+ * This test checks whether the resources are correctly made available by maven.
+ * 
+ * Created by Stephen Cerruti on 11/19/16.
+ */
+public class ResourceFileTest {
+    @Test
+    public void resourceFilePresent() throws Exception {
+        assertNotNull(PlayBatman.class.getResource("/batman.wav"));
+
+    }
+}
